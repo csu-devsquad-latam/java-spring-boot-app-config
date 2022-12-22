de87be857456258f6fa1faa831ea43102d5633d2
Vinicius Souza <visouza@microsoft.com>
2022-12-22 00:44:19 +0000

    fix: adding RestController Annotation

--- src/main/java/com/devsquad/springboot/appconfig/controllers/HelloController.java
+++ src/main/java/com/devsquad/springboot/appconfig/controllers/HelloController.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
+@RestController
 public class HelloController {
-    
-    @Autowired
-	public HelloController(){
 
+	@Autowired
+	public HelloController() {
 	}
 
 	@GetMapping(value = "/hello", produces = "application/json")
-	public ResponseEntity<String> getHelloMessage(){
-		return ResponseEntity.ok().body("hello");
+	public ResponseEntity<String> getHelloMessage() {
+		return ResponseEntity.ok().body("{\"hello\":\"world\"}");
 	}
 }
